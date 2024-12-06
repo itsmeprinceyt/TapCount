9133426c5ebbb5b70be028d9d312689bb44391ed
itsmeprinceyt <114930668+itsmeprinceyt@users.noreply.github.com>
2024-12-06 22:01:25 +0530

    Added Auto Button

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 let counter = Number(localStorage.getItem('counterNumber')) || 0;
-document.querySelector('.counter').textContent =`${counter}`;
-document.querySelector('.Tap').addEventListener('click',()=>
-{
-    counter += 1;
-    localStorage.setItem('counterNumber',counter);
-    document.querySelector('.counter').textContent =`${counter}`;
-})
+let autoIncrement = null;
 
-document.querySelector('.Reset').addEventListener('click',()=>
-{
+document.querySelector('.counter').textContent = `${counter}`;
+
+document.querySelector('.Tap').addEventListener('click', () => {
+    counter += 1;
+    localStorage.setItem('counterNumber', counter);
+    document.querySelector('.counter').textContent = `${counter}`;
+});
+
+document.querySelector('.Reset').addEventListener('click', () => {
     localStorage.removeItem('counterNumber');
     counter = 0;
-    document.querySelector('.counter').textContent =`${counter}`;
-})
+    document.querySelector('.counter').textContent = `${counter}`;
+});
+
+document.querySelector('.Auto').addEventListener('click', () => {
+    if (autoIncrement) {
+        clearInterval(autoIncrement);
+        autoIncrement = null;
+        document.querySelector('.Auto').textContent = "Start Auto";
+    } else {
+        autoIncrement = setInterval(() => {
+            counter += 1;
+            localStorage.setItem('counterNumber', counter);
+            document.querySelector('.counter').textContent = `${counter}`;
+        }, 2000);
+        document.querySelector('.Auto').textContent = "Stop Auto";
+    }
+});
